4ec9ff1946a3580daa4db2ab18d7087015100039
hsm9808 <49398226+hsm9808@users.noreply.github.com>
2020-06-11 16:13:48 +0800

    Update config.py

--- devops/config.py
+++ devops/config.py
@@ -3,12 +3,12 @@ class Config(object):
     """配置信息"""
     SECRET_KEY = "abcdefg"
     #数据库
-    SQLALCHEMY_DATABASE_URI = "mysql://root:Hsm2020!@123.56.80.143:3306/devops"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:password@123.56.80.143:3306/devops"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     #redis
     REDIS_HOST = "123.56.80.143"
     REDIS_PORT = 6379
-    REDIS_PASSWORD = "hsm123"
+    REDIS_PASSWORD = "password"
     #flask-session配置
     SESSION_TYPE = "redis"
     SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT,password=REDIS_PASSWORD)
